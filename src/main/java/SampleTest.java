ce52dbe92f0e998d789b925429e1f99333688342
Ajay Raikwar <ajayraiwar@ajayraikwarmac.local>
2020-10-07 10:01:38 +0530

    new code

--- src/main/java/SampleTest.java
+++ src/main/java/SampleTest.java
@@ -1,29 +1,78 @@
 import org.apache.spark.SparkConf;
+import org.apache.spark.SparkContext;
 import org.apache.spark.api.java.function.FilterFunction;
+import org.apache.spark.sql.DataFrameReader;
+import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.Dataset;
 
-public class SampleTest {
+import java.util.TimerTask;
+import java.util.Date;
+import java.util.Timer;
+
+
+public class SampleTest extends TimerTask {
+    public void run() {
+        System.out.println("Timer task started at:" + new Date());
+        completeTask();
+        System.out.println("Timer task finished at:" + new Date());
+    }
+
+    private void completeTask() {
+        try {
+            //assuming it takes 20 secs to complete the task
+            String providerFile = "/usr/local/providers/providers.xml";
+            String metaFile = "/usr/local/providers/providersmeta.xml";
+
+            SparkSession spark = SparkSession.builder().
+                    config("dfs.client.read.shortcircuit.skip.checksum", "true").getOrCreate();
+            Dataset<Row> df = spark.read()
+                    .format("com.databricks.spark.xml")
+                    .option("rowTag", "details")
+                    .load(providerFile);
+
+            Dataset<Row> df1 = spark.read()
+                    .format("com.databricks.spark.xml")
+                    .option("rowTag", "address")
+                    .load(metaFile);
+
+
+            df.show();
+            df1.show();
+
+            Dataset<Row> newdf = df.join(df1, "id");
+            newdf.show();
+
+            System.out.println("ajay");
+
+            System.out.println("Numbes of Rows : " + df.count());
+
+            newdf.repartition(1).write()
+                    .mode("overwrite")
+                    .format("com.databricks.spark.xml")
+                    .option("rootTag", "providers")
+                    .option("rowTag", "provider")
+                    .save("pro.xml");
+            spark.stop();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
- //       final String sparkHome = "/usr/local/spark-3.0.1-bin-hadoop2.7";
-//        SparkConf conf = new SparkConf()
-//                .setMaster("local[*]")
-//                .setAppName("spark tests")
-//                .setSparkHome(sparkHome + "/libexec");
-//        SparkSession spark = SparkSession.builder().config(conf).getOrCreate();
-        String logFile = "/usr/local/spark-3.0.1-bin-hadoop2.7/README.md";// Should be some file on your system
-        SparkSession spark = SparkSession.builder().appName("SparkTests").getOrCreate();
-        Dataset<String> logData = spark.read().textFile(logFile).cache();
 
+        TimerTask timerTask = new SampleTest();
+        //running timer task as daemon thread
+        Timer timer = new Timer(true);
+        timer.scheduleAtFixedRate(timerTask, 0, 60 * 1000);
+        System.out.println("TimerTask started");
+        //cancel after sometime
+        try {
+            Thread.sleep(120000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
-        long numAs = logData.filter((FilterFunction<String>) s -> s.contains("a")).count();
-
-
-        //logData.f
-        long numBs = logData.filter((FilterFunction<String>)s -> s.contains("b")).count();
-
-        System.out.println("Lines with a: " + numAs + ", lines with b: " + numBs);
-
-        spark.stop();
     }
 }
